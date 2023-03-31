8a5fe386a18574566a5463bc869d0d3429924cc8
Hilton Williams <hwilliams@luminoushilt.com>
2023-03-30 22:15:39 -0700

    Update backup image.

--- resources/views/article-template.blade.php
+++ resources/views/article-template.blade.php
@@ -7,12 +7,12 @@
 @section('content')
   <!-- article-template.blade.php -->
   @php
-    $featured_image = get_field('banner_image') ?? ['url' => 'https://dummyimage.com/1230x600/c71cc7/fff.jpg', 'alt' => 'Filler Image'];
+    $featured_image = get_field('banner_image') ?? ['url' => @asset('images/construct.jpg'), 'alt' => 'Something went wrong.  This page must be under construction.'];
   @endphp
 
   <section class="max-w-7xl mx-auto px-2 flex flex-col justify-center items-center">
-    <div class="w-full min-h-screen mt-0 mb-16 flex justify-center items-center" style="background-image:url({{ esc_url($featured_image['url']) }});background-repeat: no-repeat;background-size: cover;">
-      <h2 class="font-serif text-2xl sm:text-5xl text-white shadow-2xl" >
+    <div class="w-full min-h-screen mt-0 mb-16 flex justify-center items-center" style="background-image:url({{ $featured_image['url'] }});background-repeat: no-repeat;background-size: cover;">
+      <h2 class="font-serif font-bold text-2xl sm:text-5xl text-white shadow-2xl" >
         {{ the_field('page_title') }}
       </h2>
     </div>
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -7,12 +7,12 @@
 @section('content')
   <!-- front-page.blade.php -->
   @php
-    $featured_image = get_field('banner_image') ?? ['url' => 'https://dummyimage.com/1440x800/c71cc7/fff.jpg', 'alt' => 'Filler Image'];
+    $featured_image = get_field('banner_image') ?? ['url' => @asset('images/construct.jpg'), 'alt' => 'Something went wrong.  This page must be under construction.'];
     $cta_url = get_field('cta_link') ?? 'https://luminoushilt.com';
   @endphp
 
   <section class="max-w-7xl mx-auto min-h-screen mt-0 mb-10 flex flex-col justify-center items-center" style="background-image:url({{ esc_url($featured_image['url']) }});background-repeat: no-repeat;background-size: cover;">
-    <h2 class="-mt-2 mb-10 font-serif text-2xl sm:text-5xl text-white shadow-2xl">{{ the_field('page_title') }}</h2>
+    <h2 class="-mt-2 mb-10 font-serif font-bold text-2xl sm:text-5xl text-white shadow-2xl">{{ the_field('page_title') }}</h2>
 
     <a href="{{ $cta_url }}"
        class="bg-lime-500 hover:bg-lime-600 mt-56 py-7 px-12 inline-block border border-solid border-lime-600 rounded-md text-lg text-white"
